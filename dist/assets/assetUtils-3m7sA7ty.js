c23e2092e9039f55f493a4dec7bcca1a87705d00
Pomidorkaeg <egornazarko2009.09@gmail.com>
2025-04-15 21:30:21 +0300

    fix: update asset paths and build configuration

--- dist/assets/assetUtils-3m7sA7ty.js
+++
@@ -1,1 +0,0 @@
-const t=t=>{if(t.startsWith("http")||t.startsWith("data:"))return t;if(!t)return"";return`/fdfdq/${t.replace(/^\/+/,"").replace(/^dist\//,"")}`.replace(/\/+/g,"/")};export{t as g};
---
+++ dist/assets/assetUtils-VWRDWK1h.js
@@ -0,0 +1,1 @@
+const t=t=>{if(t.startsWith("http")||t.startsWith("data:"))return t;if(!t)return"";return`/fdfdq/${t.replace(/^\/+/,"").replace(/^dist\//,"")}`.replace(/([^:]\/)\/+/g,"$1")};export{t as g};
